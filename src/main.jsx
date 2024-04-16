b0066a9d9e0481b325d88057b3f8dfd2685eb5a8
Xyves <xodrixx@protonmail.com>
2024-04-16 21:40:34 +0200

    Include react routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import {
-  BrowserRouter,
-  browserProvider,
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "shop",
-    element: <Shop />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
