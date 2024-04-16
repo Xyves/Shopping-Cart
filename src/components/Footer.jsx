553c90432277476056439c062b9ef4730712c29d
Xyves <xodrixx@protonmail.com>
2024-04-16 21:40:11 +0200

    Create new components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="-bottom-1/2 fixed md:p-7 text-center w-full mx-0 my-auto bg-red-400">
+      <p>
+        © 2024 Copyright : <a href="https://github.com/Xyves">Xyves</a>
+      </p>
+    </footer>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <div className="w-full bg-red-400">
+      <p>Home</p>
+      <Footer />
+    </div>
+  );
+}
