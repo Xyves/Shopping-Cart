5db5305d0c97ef0e2c0dac4f9e8181d95cec8928
Xyves <xodrixx@protonmail.com>
2024-04-18 20:42:53 +0200

    Fix tailwind css not working, add styling to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,30 @@
 import { NavLink } from "react-router-dom";
-
+import { useState } from "react";
 export default function Header() {
+  const [cartStatus, setCartStatus] = useState(false);
+  const handleClick = () => {
+    cartStatus ? setCartStatus(false) : setCartStatus(true);
+  };
   return (
-    <header className="bg-red-600 flex w-50">
-      <ul className="no-underline list-none bg-red-700">
-        <li>
-          <NavLink to="/">Home</NavLink>
+    <header className="flex p-8 bg-red-700 ">
+      <ul className="no-underline list-none  text-lg ">
+        <li className="inline">
+          <NavLink className=" header text-lg p-3 inline" to="/">
+            Home
+          </NavLink>
         </li>
-        <li>
-          <NavLink to="/about">About</NavLink>
+        <li className="inline">
+          <NavLink to="/shop" className="p-3">
+            Shop
+          </NavLink>
         </li>
-        <li>
-          <NavLink to="/shop">Shop</NavLink>
+        <li className="inline" onClick={handleClick}>
+          Shop
+        </li>
+        <li className="inline">
+          <NavLink to="/about" className="p-3 inline">
+            About
+          </NavLink>
         </li>
       </ul>
     </header>
