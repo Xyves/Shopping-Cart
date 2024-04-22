4797878deecba1373c4d42f722db5569cd8586ce
Xyves <xodrixx@protonmail.com>
2024-04-22 19:57:01 +0200

    Create new contact route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ErrorPage from "./pages/ErrorPage";
 import Shop from "./pages/Shop";
 import About from "./pages/About";
 import MainLayout from "./Layouts/MainLayout";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route index element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/shop" element={<Shop />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
         <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
