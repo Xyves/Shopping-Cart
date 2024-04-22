cd1d1201c9fe232fef64203279ffea168a20d0e8
Xyves <xodrixx@protonmail.com>
2024-04-22 19:57:11 +0200

    Update header styling and logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,39 @@
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
 import Logo from "../assets/img/logo.png";
+import Icon from "@mdi/react";
+import { mdiBasket } from "@mdi/js";
 
 export default function Header() {
+  const navItems = [
+    ["/shop", "Shop"],
+    ["/about", "About"],
+    ["/contact", "Contact"],
+  ];
   const [cartStatus, setCartStatus] = useState(false);
   const handleClick = () => {
     cartStatus ? setCartStatus(false) : setCartStatus(true);
   };
   return (
-    <header className="flex p-8 bg-red-700 ">
-      <ul className="no-underline list-none  text-lg ">
-        <li className="inline">
-          <NavLink className=" header text-lg p-3 inline" to="/">
-            Home
-            <img src={Logo} alt="" />
-          </NavLink>
-        </li>
-        <li className="inline">
-          <NavLink to="/shop" className="p-3">
-            Shop
-          </NavLink>
-        </li>
-        <li className="inline" onClick={handleClick}>
-          Shop
-        </li>
-        <li className="inline">
-          <NavLink to="/about" className="p-3 inline">
-            About
-          </NavLink>
-        </li>
+    <header className="flex justify-center items-center  bg-[#654062] border-4 border-black border-solid">
+      <NavLink className=" header  p-1 inline mr-12" to="/">
+        <img src={Logo} alt="shop logo" className="h-28" />
+      </NavLink>
+      <ul className="text-lg  p-5 text-white">
+        {navItems.map(([url, title], index) => (
+          <li key={index} className="inline mx-7 text-3xl ">
+            <NavLink to={url} className="border-red-500">
+              {title}
+            </NavLink>
+          </li>
+        ))}
       </ul>
+      <Icon
+        path={mdiBasket}
+        size={1.4}
+        onclick={handleClick}
+        className="text-white"
+      />
     </header>
   );
 }
