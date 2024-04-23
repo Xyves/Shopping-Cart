79e44d34a4f2ce6827ed715712ae5827e35a3413
Xyves <xodrixx@protonmail.com>
2024-04-23 19:57:48 +0200

    Create contact form component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,38 @@
 import React from "react";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <main>
+        <p className="text-center">Contact</p>
+        <div className="form">
+          <form action="SENT">
+            <fieldset>
+              <legend>Contact data</legend>
+              <p>
+                <label htmlFor="firstName">First Name:</label>
+                <input type="text" placeholder="" id="firstName" />
+              </p>
+              <p>
+                <label htmlFor="surname">Surname:</label>
+                <input type="text" placeholder="" id="surname" />
+              </p>
+              <p>
+                <label htmlFor="">Email:</label>
+                <input type="text" placeholder="" id="" />
+              </p>
+              <p>
+                <label htmlFor="">Phone:</label>
+                <input type="text" placeholder="" id="" />
+              </p>
+              <p>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+              </p>
+            </fieldset>
+            <button className="text-bold">sent it</button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
 }
