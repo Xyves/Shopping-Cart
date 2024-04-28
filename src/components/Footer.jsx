07e51c6a94021c0618504f32ba4ca76fbd2cd1ee
Xyves <xodrixx@protonmail.com>
2024-04-28 21:34:09 +0200

    Update footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="-bottom-1/2 fixed md:p-7 text-center w-full mx-0 my-auto bg-red-400">
+    <footer className="bottom-0  absolute w-full p-6 md:p-7 text-center h-6 bg-red-400">
       <p>
         © 2024 Copyright : <a href="https://github.com/Xyves">Xyves</a>
       </p>
