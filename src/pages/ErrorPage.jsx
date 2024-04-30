4e074bed12106ad26d4a2bb940f12156d1ef9d33
Xyves <xodrixx@protonmail.com>
2024-04-30 22:42:23 +0200

    Fix header wrongly places

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,8 @@
 import { NavLink } from "react-router-dom";
-import Home from "./Home";
-import Header from "../components/Header";
+
 export default function ErrorPage() {
   return (
     <div>
-      <Header />
       <h1>Sorry</h1>
       <h2>We couldn&#39;t find that page</h2>
       <h3>
