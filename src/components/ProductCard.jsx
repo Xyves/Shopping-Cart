147a20108c1e49ab29c0c8c7b644747a5c12490a
Xyves <xodrixx@protonmail.com>
2024-04-30 22:41:57 +0200

    Fetch api request and render cards based on it

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-
-export default function Item() {
+const generateQuantity = () => {
+  Math.floor(Math.floor() * 9 + 1);
+};
+export default function Item(props) {
+  const { title, desc, img, price } = props;
   return (
-    <div className="itemContainer">
+    <div className="itemContainer  bg-red-600 ">
       <div className="item">
-        <div className="top-desc">
-          <img src="" alt="" />
+        <div className="top-desc w-1/2">
+          <img src={img[0]} alt="" className="w-" />
         </div>
-        <div className="bottom-desc">
-          <p>20$</p>
-          <label htmlFor="">Quantity</label>
+        <div className="bottom-desc ">
+          <p>{title}</p>
+          <p>{price}$</p>
+          <label htmlFor="quantity">Quantity 15</label>
           <input type="number" name="" id="" placeholder="1" />
           <button>Add to cart</button>
         </div>
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
 export default function Shop() {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://api.escuelajs.co/api/v1/categories/2/products?offset=0&limit=15"
+    )
+      .then((res) => res.json())
+      .then((json) => setItems(json));
+  }, []);
+
+  console.log(items);
+
   return (
-    <div>
-      <p>Shop</p>
+    <div className="flex justify-center">
+      <div className="grid justify-items-center  content-center justify-center shopContainer w-[70%] grid-cols-3	bg-yellow-200">
+        {items.map((item) => (
+          <ProductCard
+            key={item.id}
+            title={item.title}
+            price={item.price}
+            desc={item.description}
+            img={item.images}
+          ></ProductCard>
+        ))}
+        <p>Shop</p>
+      </div>
     </div>
   );
 }
