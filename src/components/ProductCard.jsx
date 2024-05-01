a0e48baa93454fe0445b0a64d3bad9ba15e2a5d4
Xyves <xodrixx@protonmail.com>
2024-05-01 21:57:35 +0200

    Create product card styling

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,19 +3,41 @@ const generateQuantity = () => {
   Math.floor(Math.floor() * 9 + 1);
 };
 export default function Item(props) {
-  const { title, desc, img, price } = props;
+  const { title, desc, img, price, description } = props;
   return (
-    <div className="itemContainer  bg-red-600 ">
-      <div className="item">
-        <div className="top-desc w-1/2">
-          <img src={img[0]} alt="" className="w-" />
+    <div className="item bg-red-700 flex  wrap justify-center ">
+      <div className=" flex mx-4 items-center w-3/4 ">
+        <div className="left-content  h-3/4 w-3/4">
+          <img src={img} alt="" className="h-full w-full" />
         </div>
-        <div className="bottom-desc ">
-          <p>{title}</p>
-          <p>{price}$</p>
-          <label htmlFor="quantity">Quantity 15</label>
-          <input type="number" name="" id="" placeholder="1" />
-          <button>Add to cart</button>
+      </div>
+      <div className="right-content my-6 wrap text-center w-3/4">
+        <p className="font-bold text-xl">{title}</p>
+        <p className="text-[11px] flex-end text-center">{desc}X</p>
+        <div className="rating"></div>
+
+        <p>{price}$</p>
+        <div className="colors mt-4">
+          <p>Choose your colors:</p>
+          <button className="rounded-full bg-black w-5 h-5 mx-2"></button>
+          <button className="rounded-full bg-white w-5 h-5"></button>
+        </div>
+        <div className="bottom-content my-5 inline">
+          <label htmlFor="quantity text-sm">Quantity:</label>
+          <input
+            type="number"
+            name="quantity"
+            min="1"
+            max="15"
+            step="1"
+            className=" ml-2 h-12"
+            placeholder="1"
+            // TBA border
+          />
+
+          <button className="bg-blue-500 p-2 rounded-md text-white inline">
+            Add to cart
+          </button>
         </div>
       </div>
     </div>
