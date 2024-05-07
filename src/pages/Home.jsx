3f38d7dd4214d129ebe32107edcc2bfa436c6fa8
Xyves <xodrixx@protonmail.com>
2024-05-07 21:45:51 +0200

    Create home page layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,23 @@
-import devteam from "../assets/img/devteam.jpg";
 import { NavLink } from "react-router-dom";
+import shoppingVector from "../assets/img/shopping-vector.jpg";
 export default function Home() {
   return (
-    <div className="w-full  flex justify-center text-black bg-main flex-1">
+    <div className="w-full  flex justify-center text-black bg-main flex-1 items-center">
       <div className="left-side h-full w-1/2">
-        <h1 className="text-center">Electro</h1>
-        <h2>Shop Smarter, Not Harder</h2>
+        <h1 className="text-[#786dff] text-7xl mb-5 word tracking-wide">
+          Electro
+        </h1>
+        <h2 className="text-blue-600 text-4xl mb-4">
+          Shop Smarter, Not Harder
+        </h2>
         <NavLink to="/shop">
-          <button>Shop now</button>
+          <button className="py-3 px-5 bg-purple-300 rounded-lg text-white">
+            Shop now
+          </button>
         </NavLink>
       </div>
-      <div className="right-side h-full w-1/2">
-        <img src={devteam} alt="Developer team" className="w-1/2 max-h-69" />
+      <div className="image w-1/3">
+        <img src={shoppingVector} alt="shopping woman" />
       </div>
     </div>
   );
