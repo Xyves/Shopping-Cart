0b9fa5545ca7983d50cefd3ea8ab523eb714d140
Xyves <xodrixx@protonmail.com>
2024-05-30 11:19:23 +0200

    Add target blank to anchor tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,11 @@ export default function Footer() {
     <footer className="p-2 md:p-4 w-full relative bottom-0 text-center clear-both bg-footer">
       <p className="text-1xl text-white">
         Copyright © 2024 Xyves &nbsp;
-        <a href="https://github.com/Xyves" className="font-bold underline">
+        <a
+          href="https://github.com/Xyves"
+          className="font-bold underline"
+          target="_blank"
+        >
           <FaGithub className="text-center inline-block"></FaGithub>
         </a>
       </p>
